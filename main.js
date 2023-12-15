b1697c33b045bfcc48addb7e08ad6d248ebafd8b
Milk Miao <mwsxh@163.com>
2023-12-15 15:25:24 +0800

    返回当前的Points

--- main.js
+++ main.js
@@ -19,6 +19,7 @@ const glados = async () => {
     return [
       'Checkin OK',
       `${checkin.message}`,
+      `Points ${checkin.list[0].balance}`,
       `Left Days ${Number(status.data.leftDays)}`,
     ]
   } catch (error) {
